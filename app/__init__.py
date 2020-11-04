5edf84ee8df16423474c6ce4f1fa704d853014af
SharonFaith <sharonfaith15@gmail.com>
2020-11-04 14:07:49 +0300

    set up configurations

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    #creating app configurations
+    app.config.from_object(config_options[config_name])
+
+    #initializing flask extensions
+    bootstrap.init_app(app)
+    #db.init_app(app)
+    #login_manager.init_app(app)
+    #mail.init_app(app)
+
+    #configure UploadSet
+    #configure_uploads(app, photos)
+
+    #registering blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
+    
+    return app
+    
