cbd053e214461cf8aafe8d97b3638b56b101f618
SharonFaith <sharonfaith15@gmail.com>
2020-11-04 14:23:55 +0300

    add index view function

---
+++ app/main/views.py
@@ -0,0 +1,12 @@
+from flask import render_template, request, redirect, url_for, abort
+from . import main
+
+
+
+
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
