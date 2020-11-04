85c25d74c338be0b2994b65c5dc741df55844655
SharonFaith <sharonfaith15@gmail.com>
2020-11-04 22:17:14 +0300

    add link to student portal

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main
+from ..models import User
 
 
 
@@ -10,3 +11,12 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@main.route('/user/<student_id>')
+def profile(student_id):
+    user = User.query.filter_by(student_id = student_id).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("student_portal.html", user = user)
