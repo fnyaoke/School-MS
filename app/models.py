99196c5dcd079943a05e31522704e48a74fec0ce
SharonFaith <sharonfaith15@gmail.com>
2020-11-04 15:51:34 +0300

    migrate to initial db schema

--- app/models.py
+++ app/models.py
@@ -9,10 +9,10 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
     email = db.Column(db.String(255))
-    contact = db.Column(db.Integer(255))
+    contact = db.Column(db.String(255))
     parentGuardian = db.Column(db.String(255))
     paymentPlan = db.Column(db.String(255))
 
 
     def __repr__(self):
-        return f'User {self.name}'
+        return f'Student {self.name}'
