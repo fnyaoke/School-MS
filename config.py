771771a4a872c64b5bdde84221580b62b5646b38
SharonFaith <sharonfaith15@gmail.com>
2020-11-05 02:29:19 +0300

    remove text from index.html

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://student:Amazeing16@localhost/school_ms'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    email configurations
+
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
